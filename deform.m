1d1f736ad93e6da29668d40968a4df98e40451f8
kevinecahill <kevinecahill@gmail.com>
2018-11-02 17:07:06 -0600

    new deform.m

--- deform.m
+++ deform.m
@@ -3,7 +3,7 @@ close all
 clc
 gap = 0.05
 x = 0:.001:2*pi;
-fs = 14; sf = 16; hside = 2; r = 1.5;
+fs = 14; sf = 16; hside = 2; r = 1.75;
 x1 = 1/2; x2 = 2;
 th1 = atan(1/x1); th2 = atan(1/x2);
 r1 = sqrt(1 + x1^2); r2 = sqrt(1 + x2^2);
@@ -18,7 +18,7 @@ text('Interpreter','latex','String','$\wedge$','Position',[r1-x1-gap,0],'FontSiz
 text('Interpreter','latex','String','$\wedge$','Position',[-r1+x1-gap,0],'FontSize',sf)
 text('Interpreter','latex','String','$\wedge$','Position',[r2-x2-gap,0],'FontSize',sf)
 text('Interpreter','latex','String','$\wedge$','Position',[-r2+x2-gap,0],'FontSize',sf)
-text('Interpreter','latex','String','region of analyticity','Position',[-13*gap,-1.75],'FontSize',fs)
+text('Interpreter','latex','String','region of analyticity','Position',[-10.5*gap,-1.40],'FontSize',fs)
 plot(0,1,'k.','markersize',10)
 plot(0,-1,'k.','markersize',10)
 plot(r*cos(x),r*sin(x),'k-','linewidth',2)
